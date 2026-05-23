7a338f26c25278b5744562b52537fb2565706d94
Mohammed Muddassir Ali <studymuddassir@gmail.com>
2026-05-23 13:10:04 +0000

    fixed test on ci using vitest

---
+++ src/tests/.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test } from "vitest";
+
+const person = {
+  isActive: true,
+  age: 32,
+};
+
+describe("person", () => {
+
+  test("person is defined", () => {
+    expect(person).toBeDefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+
+  test("is age > 18", ()=>{
+    expect(person.age).toBeGreaterThan(18);
+  })
+
+});
+
+describe("fixed add test", () => {
+  test("should fail", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
