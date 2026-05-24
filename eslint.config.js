8c47d92d239823ac0fc325fb7126fccf37e2d86b
Mohammed Muddassir Ali <studymuddassir@gmail.com>
2026-05-24 06:32:24 +0000

    added security check eslint - so no ci.yml update needed

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,30 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
-export default [
+export default defineConfig([
   {
     ignores: ["dist/**", "coverage/**", "node_modules/**"],
   },
 
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    ...js.configs.recommended,
+
+    plugins: {
+      js,
+      security: pluginSecurity,
+    },
+
+    extends: ["js/recommended"],
+
     languageOptions: {
-      globals: globals.browser,
+      globals: globals.node,
     },
   },
 
   ...tseslint.configs.recommended,
-];
+  pluginSecurity.configs.recommended,
+]);
+
