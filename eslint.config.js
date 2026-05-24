73a21edaafa62973f34003c4457b43177d4f1099
Mohammed Muddassir Ali <studymuddassir@gmail.com>
2026-05-24 05:29:38 +0000

    updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,6 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser },  ignores: ["dist/**", "coverage/**", "node_modules/**"], },
   tseslint.configs.recommended,
 ]);
